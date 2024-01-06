03a697b1997a454e48bf7e42300892dfc781ffb8
Roshith PB <103378908+pb-roshith@users.noreply.github.com>
2024-01-06 21:29:24 +0530

    Create traffic.ino

---
+++ traffic.ino
@@ -0,0 +1,69 @@
+const int r = 9;
+const int y = 8;
+const int g = 7;
+
+void setup() {
+  pinMode(r,OUTPUT);
+  pinMode(y,OUTPUT);
+  pinMode(g,OUTPUT);
+}
+
+void loop() {
+  digitalWrite(r, HIGH);
+  delay(1000);
+  digitalWrite(r, LOW);
+
+  digitalWrite(y, HIGH);
+  delay(1000);
+  digitalWrite(y, LOW);
+  delay(1000);
+
+  digitalWrite(y, HIGH);
+  delay(1000);
+  digitalWrite(y, LOW);
+  delay(1000);
+
+  digitalWrite(y, HIGH);
+  delay(1000);
+  digitalWrite(y, LOW);
+  delay(1000);
+
+  digitalWrite(y, HIGH);
+  delay(1000);
+  digitalWrite(y, LOW);
+  delay(1000);
+
+  digitalWrite(y, HIGH);
+  delay(1000);
+  digitalWrite(y, LOW);
+  delay(1000);
+
+  digitalWrite(g, HIGH);
+  delay(1000);
+  digitalWrite(g, LOW);
+
+  digitalWrite(y, HIGH);
+  delay(1000);
+  digitalWrite(y, LOW);
+  delay(1000);
+
+  digitalWrite(y, HIGH);
+  delay(1000);
+  digitalWrite(y, LOW);
+  delay(1000);
+
+  digitalWrite(y, HIGH);
+  delay(1000);
+  digitalWrite(y, LOW);
+  delay(1000);
+
+  digitalWrite(y, HIGH);
+  delay(1000);
+  digitalWrite(y, LOW);
+  delay(1000);
+
+  digitalWrite(y, HIGH);
+  delay(1000);
+  digitalWrite(y, LOW);
+  delay(1000);
+}
